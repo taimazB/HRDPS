a0b36384ce0d6f367954d7f908893f7f367fdbef
taimaz <tb4764@mun.ca>
2024-06-26 15:18:01 -0230

    configs updated.

--- configs.sh
+++ configs.sh
@@ -1,5 +1,5 @@
 export MODEL=HRDPS
 export MAIN=/home/taimaz/Projects/models/${MODEL}
 export SERVER_IP=process.oceangns.com
-export SERVER_PORT=4122
+export SERVER_PORT=22
 export SERVER_DIR=/home/taimaz/models/canadaClim/${MODEL}
